846b777bccf09aab8443f52b3a2f19173b38ba86
heshanka <info@heshanka.com>
2022-04-24 12:01:05 +0300

    Added constants class to hold repeating values

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+
+  static const primaryColor = Color(0xff3b2b53);
+
+  static const secondaryColor = Color(0xffdbabe3);
+
+  static const disabledBtnColor = Color(0xd0d4cfdb);
+
+  static const buttonLightTextStyle = TextStyle(color: Colors.white, fontFamily: 'Raleway', fontSize: 16);
+
+  static const buttonDarkTextStyle = TextStyle(color: Colors.black, fontFamily: 'Raleway', fontSize: 16);
+
+}
